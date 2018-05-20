dfd5deb5041a27f2b959cdfcf057d72f54c59901
Brian Dixon <briandixon@Brians-Mac-mini.home>
2018-05-20 05:07:16 -0400

    making better changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,54 +1,78 @@
 'use strict';
 $(document).ready(function() {
-  startTitle(stuffMaker)
+  startTitle()
   //startTitle(stuffItem.create())
   deleteBookmark()
 })
 
+///test functions for rendering
+/////////
+const bookstore = [
+  {name: " "}
+];
 
-const BASE_URL = 'https://thinkful-list-api.herokuapp.com/brian'
+
+
+$(".listrender").html(`<form id="test-list">
+  <input class = 'testinput'></input>
+  <button class ='submit'>submit</button>
+
+</form>`)
 
 function deleteBookmark(){
 
    $(".remove").on('click', function(){
-    $(".bookmark").remove().closest();
+    $(".booklist-controls").remove().closest();
    console.log("hi");
   });
 
  }
- ///test functions for rendering
-/////////
+
  function startTitle(cb) {
-   $(".submit").click(function() {
-     console.log("hello")
+   $("#test-list").submit(function(event) {
+     event.preventDefault();
+     //console.log("hello")
 
      let newinput = $(".testinput").val()
 
-     console.log("hi how")
-     cb(newinput)
+    // console.log("hi how")
+     //cb(newinput)
+     $(".testinput").val()
 
-     return newinput
+     $(".listtwo").append(
+       `<li>
+                <div class ="booklist-controls">
+                <h3>${newinput}</h3>
+               <h4>description<h4>
+               <h4>link<h4>
+               <button class= "view">view</button>
+               <button class= "remove">remove</button>
+               </div>
+        </li>`
+     )
+     //return newinput
    })
  }
 
  //
- function stuffMaker(newinput) {
-   return $(".listtwo").html(
-     `<li>
-              <div class ="booklist-controls">
-              <h3>${newinput}</h3>
-             <h4>description<h4>
-             <h4>link<h4>
-             <button class= "view">view</button>
-             <button class= "remove">remove</button>
-             </div>
-             </li>`
-   )
-   console.log("ran")
- }
+ // function stuffMaker(newinput) {
+ //   return $(".listtwo").html(
+ //     `<li>
+ //              <div class ="booklist-controls">
+ //              <h3>${newinput}</h3>
+ //             <h4>description<h4>
+ //             <h4>link<h4>
+ //             <button class= "view">view</button>
+ //             <button class= "remove">remove</button>
+ //             </div>
+ //             </li>`
+ //   )
+ //   console.log("ran")
+ // }
 
 /////////////
 
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/brian/bookmarks'
 
 
 
