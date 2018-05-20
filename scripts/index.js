5a4befcb4c91b1dd38e96a95e4e55c6eeaad3a44
Brian Dixon <briandixon@Brians-Mac-mini.home>
2018-05-20 13:53:06 -0400

    2nd change for now

--- scripts/index.js
+++ scripts/index.js
@@ -3,13 +3,13 @@ $(document).ready(function() {
   startTitle()
   //startTitle(stuffItem.create())
   deleteBookmark()
+  //handlebooklist()
 })
+
 
 ///test functions for rendering
 /////////
-const bookstore = [
-  {name: " "}
-];
+
 
 
 
@@ -39,125 +39,52 @@ function deleteBookmark(){
      //cb(newinput)
      $(".testinput").val()
 
-     $(".listtwo").append(
-       `<li>
-                <div class ="booklist-controls">
-                <h3>${newinput}</h3>
-               <h4>description<h4>
-               <h4>link<h4>
-               <button class= "view">view</button>
-               <button class= "remove">remove</button>
-               </div>
-        </li>`
-     )
+     // $(".listtwo").append(
+     //   `<li>
+     //            <div class ="booklist-controls">
+     //            <h3>${item.name}</h3>
+     //           <h4>description<h4>
+     //           <h4>link<h4>
+     //           <button class= "view">view</button>
+     //           <button class= "remove">remove</button>
+     //           </div>
+     //    </li>`
+     // )
      //return newinput
    })
  }
+ const bookstore = [
+   {name: "more", }
+ ];
 
- //
- // function stuffMaker(newinput) {
- //   return $(".listtwo").html(
- //     `<li>
- //              <div class ="booklist-controls">
- //              <h3>${newinput}</h3>
- //             <h4>description<h4>
- //             <h4>link<h4>
- //             <button class= "view">view</button>
- //             <button class= "remove">remove</button>
- //             </div>
- //             </li>`
- //   )
- //   console.log("ran")
- // }
+//passing bookstore in as paramater, no side effects
+function generateBookmarkString(store){
+    const items = store.map((item, index) =>
+    generateitemTemplate(item, index));
+    console.log("generatedbookmarkstring");
+    return items.join("");
+}
+
+
+ function generateitemTemplate(item, itemindex) {
+   console.log("shopping list generated")
+   return  `<li>${item.name}</li>`
+
+ }
+
+function renderBookmarklist(){
+console.log("render booklist")
+const bookmarksItemString = generateBookmarkString(bookstore);
+  $('.listtwo').html(bookmarksItemString);
+}
+
+function handlebooklist(){
+  renderBookmarklist()
+  generateitemTemplate()
+}
+
+$(handlebooklist());
 
 /////////////
 
 const BASE_URL = 'https://thinkful-list-api.herokuapp.com/brian/bookmarks'
-
-
-
-
-
- const store = {
-   bookmarks: []
- };
-
-
-
-
-
-
-
- const decorateResponse = function(response) {
-   return response.items.map(function(item) {
-     return {
-
-       title: item.title
-
-     };
-   });
- };
-
- const postBookmark = function(sendTerm, callback) {
-  $.postJSON(BASE_URL,
-     {
-       name: sendTerm,
-
-     },
-     callback
-   );
- };
-
-
-
-
-
-
-
- const generateBookmarkItemHtml = function(books) {
-console.log(video);
-   return `<li>${title}</li>`
-
- };
-
-
- const addbookmarksToStore = function(bookmarks) {
-   store.bookmarks = bookmarks;
-   console.log(store.bookmarks);
- };
-
-///is this right??
- const render = function() {
-   console.log(store.bookmarks);
-   let bookmarksHtmlArrays = store.bookmarks.map(function(books) {
-     return generateBookmarkItemHtml(books);
-   });
-   console.log(bookmarksHtmlArrays);
-   const bookmarkHtmlString = bookmarksHtmlArrays.join('');
-   console.log(bookmarkHtmlString);
-   $('.results').html(bookmarkHtmlString);
- };
-
-
-
-
-///handle function for submiting and rendering
- const handleFormSubmit = function() {
-   $('form').submit(function(event) {
-     event.preventDefault();
-     const makeBookmark = $('.setBookName').val();
-
-     $(event.currentTarget).parent().find('.setBookName').val('');
-     postBookmark(sendTerm, function(response) {
-       console.log(response);
-       let decoratedResponse = decorateResponse(response);
-       console.log(decoratedResponse);
-       addVideosToStore(decoratedResponse);
-       render();
-     });
-   });
- };
-
- $(function () {
-   handleFormSubmit();
- });
