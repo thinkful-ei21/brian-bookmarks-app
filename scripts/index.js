375a29a54f6ead0f1c55dfe6b7cc4421ecd74443
Brian Dixon <briandixon@Brians-Mac-mini.home>
2018-05-24 05:39:31 -0400

    more stuff

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,19 @@
 'use strict';
 ///test functions for rendering
 /////////
-function addItemTobooklist(itemName){
-  bookstore.push({title:itemName});
-  bookstore.push({url:itemurl});
+function addItemTobooklist(itemName,itemurl){
+  bookstore.push(
+
+    {
+      title:itemName,
+      url:itemurl,
+      isExpanded:false
+    }
+
+  );
+  //bookstore.push({url:itemurl});
 }
 
-
-
-
-
-function deleteBookmark(){
-
-   $(".remove").on('click', function(){
-    $(".js-Item-index-element").remove().closest();
-   console.log("hi");
-  });
-
- }
 
  function handleNewSubmit(event) {
    $('#list').submit(function(event) {
@@ -28,35 +24,43 @@ function deleteBookmark(){
      console.log(newtitle);
      $(".link-input").val('');
      $(".title-input").val('');
-     addItemTobooklist(newtitle);
-     postBookmark(newtitle);
-     renderBookmarklist();
+
+     postBookmark(newtitle,newurl, function(item){
+       console.log(item);
+       addItemTobooklist(newtitle,newurl);
+       renderBookmarklist();
+     });
+
    });
   }
 
 
  let bookstore = [
    {title:"more",
-    url: "https://www.google.com/"}
+    url: "https://www.google.com/",
+    isExpanded:false
+   }
  ];
 
 //passing bookstore in as paramater, no side effects
 function generateBookmarkString(store){
-    const items = store.map((item, index) =>
-    generateitemTemplate(item, index));
+    const items = store.map(generateBookmarkView);
     console.log("generatedbookmarkstring");
     return items.join("");
 }
 
 
- function generateitemTemplate(item, itemindex) {
+
+
+
+ function generateBookmarkCondensedView(item, itemindex) {
    console.log("shopping list generated")
-   return  `<li class= 'js-Item-index-element'
+   return  `<li class= 'js-Item-index-condense'
                 data-item-index= '${itemindex}'>
                <div class ="booklist-controls">
                <h3>'${item.title}'</h3>
+               <h3>${item.url}</h3>
 
-               <h4>'${item.url}'<h4>
                <button class= "view">view</button>
                <button class= "remove">remove</button>
                </div>
@@ -64,17 +68,58 @@ function generateBookmarkString(store){
 
  }
 
+ function generateBookmarkExpandedView(item, itemindex) {
+   console.log("shopping list generated")
+   return  `<li class= 'js-Item-index-expand'
+                data-item-index= '${itemindex}'>
+               <h1>stuff</h1>
+         </li>`
+
+ }
+
+ function bookview(){
+   $(".view").on('click', generateBookmarkView(bookstore => {
+     item.isExpanded = true
+     console.log("bookview");
+   }))
+ }
+
+ function generateBookmarkView(item, itemindex){
+   if (item.isExpanded){
+     return generateBookmarkExpandedView(item, itemindex);
+   } else {
+     return generateBookmarkCondensedView(item, itemindex);
+   }
+ }
+
+ function deleteBookmark(){
+    $(".listrender").on('click', function(){
+      $('.remove').on('click', function(){
+        console.log('delete')
+     $(".js-Item-index-condense").remove().closest();
+
+   });
+ });
+
+  }
+//JSON.stringify
 /////////API CALLS////////
 const BASE_URL = 'https://thinkful-list-api.herokuapp.com/brian/bookmarks'
 
 function postBookmark(title,url,callback){
-  $.post(BASE_URL,
-    {
-      title,
-      url
-    },
-    callback
-  )
+  console.log(`${title} ${url}`)
+  $.ajax({
+      url: BASE_URL,
+      data: JSON.stringify({
+        title:title,
+        url:url,
+      }),
+      contentType: 'application/json; charset=utf-8',
+      dataType: "json",
+      success: callback,
+      type: 'post'
+
+    });
 }
 
 function getBookmarks(callback){
@@ -86,7 +131,7 @@ function getBookmarks(callback){
 function renderBookmarklist(){
 console.log("render booklist")
 const bookmarksItemString = generateBookmarkString(bookstore);
-  $('.listtwo').html(bookmarksItemString);
+  $('.listrender').html(bookmarksItemString);
 }
 //////////////entry point////
 
@@ -99,7 +144,7 @@ function main(){
 
   handleNewSubmit();
   deleteBookmark();
-  handleNewSubmit();
+  bookview();
 }
 
 main();
